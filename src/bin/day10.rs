498216af75b55fe2250863dde8e64b5cbbab186c
svartalf <self@svartalf.info>
2017-12-11 01:35:34 +0300

    Day 10 part 1

---
+++ src/bin/day10.rs
@@ -0,0 +1,13 @@
+extern crate aoc2017;
+
+use aoc2017::days::day10;
+
+fn main() {
+    let input = aoc2017::read("input/day10.txt").unwrap();
+    let mut digits = (0..256).collect::<Vec<usize>>();
+    let value = day10::parse(&mut digits, &input);
+    println!("Day 10 part 1 value: {}", value);
+//    let (score, garbage) = day09::parse(&input);
+//    println!("Day 09 part 1 score: {}", score);
+//    println!("Day 09 part 2 garbage: {}", garbage);
+}
---
+++ src/days/day10/part1.rs
@@ -0,0 +1,39 @@
+pub fn parse(list: &mut Vec<usize>, input: &str) -> usize {
+    let mut current = 0usize;
+    let mut skip_size = 0usize;
+    let lengths: Vec<usize> = input.split(",").map(|part| {
+        part.parse::<usize>().unwrap()
+    }).collect();
+
+    for length in lengths {
+        let (indexes, mut values): (Vec<_>, Vec<_>) = list.iter()
+            .enumerate().cycle()
+            .skip(current).take(length)
+            .map(|(idx, item)| (idx, *item))
+            .unzip();
+        values.reverse();
+
+        for (index, value) in indexes.iter().zip(values) {
+            list[*index] = value;
+        }
+
+        current += length + skip_size;
+        skip_size += 1;
+    }
+
+    list.iter().take(2).fold(1, |acc, value| acc * value)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse;
+
+    #[test]
+    fn day10_part1_test1() {
+        let mut list: Vec<usize> = vec![0, 1, 2, 3, 4];
+        let input = "3,4,1,5";
+
+        assert_eq!(12, parse(&mut list, input));
+    }
+
+}
